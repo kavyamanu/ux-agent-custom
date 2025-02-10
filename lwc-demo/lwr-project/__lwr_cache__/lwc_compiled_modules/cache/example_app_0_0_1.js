13ab0db6b5fc802a1a8015e57b52af0d0d74a808
Kavya Manupati <kavyamanupati11@gmail.com>
2025-02-10 14:55:49 +0530

    Initial commit

---
+++ lwc-demo/lwr-project/__lwr_cache__/lwc_compiled_modules/cache/example_app_0_0_1.js
@@ -0,0 +1,11 @@
+import _tmpl from "./app.html";
+import { LightningElement, registerComponent as _registerComponent } from 'lwc';
+class HelloWorldApp extends LightningElement {
+  /*LWC compiler v7.2.0*/
+}
+const __lwc_component_class_internal = _registerComponent(HelloWorldApp, {
+  tmpl: _tmpl,
+  sel: "example-app",
+  apiVersion: 62
+});
+export default __lwc_component_class_internal;
